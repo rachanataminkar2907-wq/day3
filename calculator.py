3f9b3edc993497da4aca9584ebf0c8e2bcc9f8c5
rachanataminkar2907-wq <rachanataminkar2907@gmail.com>
2026-06-03 11:34:08 +0530

    mul and div

--- calculator.py
+++ calculator.py
@@ -3,3 +3,11 @@ def add(a, b):
 
 def sub(a, b):
     return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,6 @@
 # import pytest
-from calculator import add,sub
+from calculator import add,sub,mul,div
+
 
 def test_add():
     assert add(4, 5) == 9
@@ -10,3 +11,16 @@ def test_sub():
     assert sub(4, 5) == -1
     assert sub(2, 1) == 1
     assert sub(0, 0) == 0
+
+def test_mul():
+    assert mul(4, 5) == 20
+    assert mul(2, 3) == 6
+    assert mul(0, 5) == 0
+
+def test_div():
+    assert div(10, 2) == 5
+    assert div(9, 3) == 3
+    try:
+        div(5, 0)
+    except ValueError as e:
+        assert str(e) == "Cannot divide by zero"
