ffb287d4ebd263269c28038f76018382beacca64
rachanataminkar2907-wq <rachanataminkar2907@gmail.com>
2026-06-03 10:50:05 +0530

    added ci

--- test_calculator.py
+++ test_calculator.py
@@ -1,9 +1,12 @@
-import pytest
-from calculator import add
+# import pytest
+from calculator import add,sub
 
 def test_add():
     assert add(4, 5) == 9
     assert add(1, 2) == 3
     assert add(0, 0) == 0
 
-
+def test_sub():
+    assert sub(4, 5) == -1
+    assert sub(2, 1) == 1
+    assert sub(0, 0) == 0
